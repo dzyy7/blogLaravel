39d72e6d8eb193c4b8251a4c23ce21d98d0a7e9e
dzyyy7 <muh.dzakyau@gmail.com>
2025-08-31 21:15:51 +0700

    Implement post model and views for displaying blog posts and single post details

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,22 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 Route::get('/posts', function () {
-    $posts = [
-        ['title' => 'Judul Artikel 1',
-        'author' => 'Muhammad Dzaky',
-        'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae saepe dolorum
-                vel vitae quas iusto
-                tenetur labore dolores amet temporibus, ad eaque sed possimus quibusdam. Voluptate dolore incidunt nam
-                ad!'
-        ],
-        ['title' => 'Judul Artikel 2',
-        'author' => 'Muhammad Dzaky',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem est fuga amet
-                sequi, libero voluptatibus provident non laborum. Facilis exercitationem sit quaerat doloremque fuga.
-                Deserunt sunt neque tenetur debitis quam.'
-        ],
-
-    ];
+    $posts = Post::all();
     return view('posts', ['title' => 'Blog Page','posts' => $posts]);
+});
+Route::get('/posts/{slug}', function ($slug) {
+   $post = Post::find($slug);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+
 });
 Route::get('/about', function () {
     return view('about', ['title' => 'About Page']);
